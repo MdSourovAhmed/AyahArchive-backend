9a29c7d4a184e562516c7199fecbfa14fe04cbfe
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-08-29 23:52:51 +0600

    Update QuranVerse.js

--- models/QuranVerse.js
+++ models/QuranVerse.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const quranVerseSchema = new mongoose.Schema({
-//   surahNumber: { type: Number, required: true },
-//   surahNameArabic: { type: String, required: true },
-//   surahNameEnglish: { type: String, required: true },
-//   revelationType: { type: String, enum: ['meccan', 'medinan'], required: true },
-//   verseNumber: { type: Number, required: true },
-//   arabicText: { type: String, required: true },
-//   englishTranslation: { type: String, required: true }
-// }, { collection: 'quranVerses' });
-
-// quranVerseSchema.index({ surahNumber: 1, verseNumber: 1 });
-// quranVerseSchema.index({ surahNameEnglish: 'text' });
-
-// module.exports = mongoose.model('QuranVerse', quranVerseSchema);
-
 const mongoose = require('mongoose');
 
 const quranVerseSchema = new mongoose.Schema({
